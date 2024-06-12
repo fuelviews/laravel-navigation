f43fcb4f985c94ed2462a0849c6af57e7c82363e
thejmitchener <thejmitchener@users.noreply.github.com>
2024-06-12 19:33:34 +0000

    Fix styling

--- src/Commands/NavigationInstallCommand.php
+++ src/Commands/NavigationInstallCommand.php
@@ -3,9 +3,9 @@
 namespace Fuelviews\Navigation\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
-use Illuminate\Support\Facades\File;
 
 class NavigationInstallCommand extends Command
 {
