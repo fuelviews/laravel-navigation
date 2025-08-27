37748af41b0a7847e70bf9716403c776892ef40d
Danny Clark <67076156+sweatybreeze@users.noreply.github.com>
2025-08-27 12:36:33 -0400

    remove desktop scroll link transitions (#59)

--- resources/views/components/desktop/desktop-navigation-link.blade.php
+++ resources/views/components/desktop/desktop-navigation-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
     $classes = ($active ?? false)
-        ? 'inline-flex items-center border-b-2 border-prime-400 text-sm font-medium leading-5 hover:text-gray-400 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out py-2 uppercase'
-        : 'inline-flex items-center border-b-2 border-transparent text-sm font-medium leading-5 hover:text-gray-400 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out py-2 uppercase';
+        ? 'inline-flex items-center border-b-2 border-prime-400 text-sm font-medium leading-5 hover:text-gray-400 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 ease-in-out py-2 uppercase'
+        : 'inline-flex items-center border-b-2 border-transparent text-sm font-medium leading-5 hover:text-gray-400 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 ease-in-out py-2 uppercase';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
